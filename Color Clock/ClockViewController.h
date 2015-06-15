36938e827408d85fbf9f3be60907017a39160196
diogommartins <diogo.martins@unirio.br>
2015-06-15 02:43:44 -0300

    Added the main controller with implements the DMClockDelegate and performs the background changes

---
+++ Color Clock/ClockViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  Colour Clock
+//
+//  Created by Diogo Martins on 6/11/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DMClock.h"
+
+@interface ClockViewController : UIViewController <DMClockDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *shadowLayerView;
+@property (weak, nonatomic) IBOutlet UILabel *lblHours;
+@property (weak, nonatomic) IBOutlet UILabel *lblMinutes;
+@property (weak, nonatomic) IBOutlet UILabel *lblSeconds;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *lblDividers;
+
+@property (strong, nonatomic) DMClock * clock;
+
+@end
+
---
+++ Color Clock/ClockViewController.m
@@ -0,0 +1,77 @@
+//
+//  ViewController.m
+//  Colour Clock
+//
+//  Created by Diogo Martins on 6/11/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#import "ClockViewController.h"
+#define BRIGHTNESS_INCREASE 10
+
+@interface ClockViewController ()
+
+-(void)dropShadowOnView: (UIView *)view;
+-(void) updateLabels;
+-(void) updateBackgroundColor;
+-(CGFloat)colorFromInt: (int)value;
+
+@end
+
+@implementation ClockViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self dropShadowOnView: self.shadowLayerView];
+    self.clock = [[DMClock alloc] initWithTimeInterval:1.0 delegate: self];
+    [self.clock start];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)dropShadowOnView:(UIView *)view
+{
+    CALayer *shadowLayer = [CALayer layer];
+    shadowLayer.masksToBounds = YES;
+    shadowLayer.shadowOffset = CGSizeMake(0, 3);
+    shadowLayer.shadowRadius = 100.0f;
+    shadowLayer.shadowOpacity = 0.8f;
+    shadowLayer.shadowColor = [UIColor whiteColor].CGColor;
+    shadowLayer.shadowPath = [UIBezierPath bezierPathWithRect: view.bounds].CGPath;
+    shadowLayer.cornerRadius = 3; // if you like rounded corners
+    shadowLayer.frame = view.layer.bounds;
+    
+    [view.layer addSublayer:shadowLayer];
+}
+
+-(void)updateLabels
+{
+    [self.lblHours setText: [NSString stringWithFormat:@"%02i", self.clock.hours]];
+    [self.lblMinutes setText: [NSString stringWithFormat:@"%02i", self.clock.minutes]];
+    [self.lblSeconds setText: [NSString stringWithFormat:@"%02i", self.clock.seconds]];
+}
+
+-(CGFloat)colorFromInt:(int)value
+{
+    return value / 255.0 * BRIGHTNESS_INCREASE;
+}
+
+-(void)updateBackgroundColor
+{
+    UIColor * color = [UIColor colorWithRed: [self colorFromInt:self.clock.hours]
+                                      green: [self colorFromInt:self.clock.minutes]
+                                       blue: [self colorFromInt:self.clock.seconds]
+                                      alpha: 10.0];
+    [self.view setBackgroundColor: color];
+}
+
+-(void)clockDidUpdate
+{
+    [self updateLabels];
+    [self updateBackgroundColor];
+}
+
+@end
